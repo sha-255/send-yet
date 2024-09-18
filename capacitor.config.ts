96245e7fe7f3949e9a31640bdae5e634896d1f1b
sha-255 <fawsgre@mail.ru>
2024-09-18 10:35:03 +0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sending-yet',
+  webDir: 'dist'
+};
+
+export default config;
